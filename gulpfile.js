41164b05c759ca73e4edf5edcd775044eb0ac110
kimihirooono <kimihiro@kimihirooono-no-Mac-mini.local>
2017-02-13 15:02:52 +0900

    commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var sass = require("gulp-sass");
+var browserSync = require('browser-sync').create();
+
+//自動リロード
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
+});
+
+//ブラウザリロード
+gulp.task('bs-reload', function () {
+	browserSync.reload();
+});
+
+//sassコンパイル
+gulp.task("sass", function() {
+	gulp.src("./sass/**/*.scss")
+		.pipe(sass())
+		.pipe(gulp.dest("./css"))
+		.pipe(browser.reload({stream:true}))
+});
+
+
+
+
+gulp.task('default', ['browser-sync'], function () {
+	gulp.watch("./**/*.scss", ['sass']);
+	gulp.watch("./**/*.css", ['bs-reload']);
+	gulp.watch("./**/*.html", ['bs-reload']);
+});
